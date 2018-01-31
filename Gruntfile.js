edb23744c3d06437a3812b92200c89651c257a04
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2018-01-31 23:42:27 +0530

    Gulp Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports=function(grunt){
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
     grunt.initConfig({
         sass: {
             dist: {
@@ -28,8 +30,113 @@ module.exports=function(grunt){
                     baseDir: './'
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['img/*.{jpg,png,gif}'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                seperator: ';'
+            },
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src:[
+                        'dist/css/*.css',
+                        'dist/js/*.js'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist','dist/css','dist/js']
+            }
         }
     });
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default',['browserSync', 'watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin'
+    ]);
+    grunt.registerTask('test',['concat','cssmin','uglify']);
 }
